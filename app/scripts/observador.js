6cf731d794cd595709da1cf14f494ce5c25bb76f
Yuri Thomas <yuri.tpinheiro@gmail.com>
2016-05-04 00:25:04 -0300

    Tasks completos em busca de bugs

---
+++ app/scripts/observador.js
@@ -0,0 +1,20 @@
+
+var G = [[0.9935, 0], [0.00656, 0.9935]];
+var G2 = numeric.dot(G, G);
+var I = [[1, 0], [0, 1]];
+var Wo = [[0.0000, 1.0000], [0.00656, 0.9935]];
+var C = [[0, 1]];
+
+function L(polos) {
+    return (numeric.dot(numeric.dot(qc(polos), numeric.inv(Wo)), numeric.transpose(C)));
+}
+
+function polos(L) {
+    return numeric.eig(numeric.sub(G, numeric.dot(L, C))).lambda;
+}
+
+function qc(polos) {
+    var b = Number(polos.x[0]) + Number(polos.x[1]);
+    var c = Number(polos.x[0]) * Number(polos.x[1]) + Number(polos.y[0]) * Number(polos.y[1]);
+    return numeric.add(numeric.add(G2, numeric.mul(b,G)), numeric.mul(c, I));
+}
